e4f4e479ec4867678ac596435b2c500acda80a2a
Elia Gentili <eliagentili@gmail.com>
2016-09-15 19:24:39 +0200

    Integrates cleave.js angular directive

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import * as angular from 'angular';
 import 'angular-formly';
 
-import { AngularCleavejsController } from './controllers/angular-cleavejs.controller';
-import { AngularCleavejsService } from './services/angular-cleavejs.service';
-
 export default angular
-  .module('eg-angular-formly-cleavejs', [ 'formly' ])
-  .component('angularCleavejs', {
-    bindings: {
-      id: '<',
-      ngModel: '=',
-      to: '<',
-    },
-    controller: AngularCleavejsController,
-    template: `<input class="form-control"
-                      id="{{ $ctrl.id }}"
-                      name="{{ $ctrl.id }}"
-                      placeholder="{{ $ctrl.to.placeholder }}"
-                      formly-focus="{{ $ctrl.to.focus }}"
-                      ng-disabled="$ctrl.to.disabled"
-                      ng-model="$ctrl.ngModel"
-                      ng-required="$ctrl.to.required"
-                      ng-change="$ctrl.onChange($ctrl.ngModel)"
-                      ng-keydown="$ctrl.onKeyDown($event)">`
-  })
+  .module('eg-angular-formly-cleavejs', [ 'formly', 'cleave.js' ])
   .run(['formlyConfig', (formlyConfig) => {
     formlyConfig.setType({
       name: 'angular-cleavejs',
-      extends: 'input',
-      template: `
-      <angular-cleavejs id="id"
-                        ng-model="model[options.key]"
-                        to="to">
-      </angular-cleavejs>`,
+      defaultOptions: {
+        defaultValue: '',
+      },
+      template: `<input class="form-control" ng-model="model[options.key]" cleave="to.cleavejs">`,
+      wrapper: ['bootstrapLabel', 'bootstrapHasError'],
     });
   }])
-  .service('angularCleavejsService', AngularCleavejsService)
   .name;
